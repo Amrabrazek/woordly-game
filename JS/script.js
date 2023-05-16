b1302167250aa04d18463d4420c301f2e4f63d40
AmrAbrazek <amr.3brazek@gmail.com>
2023-05-17 00:22:00 +0300

    building with javascript

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,27 @@ addGlobalEventListener('click', ".btnx", e => {
     value = clicked.innerText;
     console.log(value)
 })
+
+{/* <div class="char col ">
+                    <button type="button" id="Q" class="btnx" >Q</button>
+                </div> */}
+
+
+keyboardfirstrow=["Q","W","E","R","T",'Y','U','I','O','P']
+keyboardfirstrow.forEach(element => addkey(element, "first-row"));
+keyboardsecondrow=['A','S','D','F','G','H','J','K','L']
+keyboardsecondrow.forEach(element => addkey(element, "second-row"));
+
+function addkey (key, row) {
+    buttuncont = document.createElement('div')
+    buttuncont.classList.add('char', 'col')
+    butt = document.createElement('button')
+    butt.classList.add('btnx')
+    butt.id = key
+    butt.type = 'button'
+    butt.innerText = key
+    buttuncont.appendChild(butt)
+
+    row = document.querySelector(`.${row}`)
+    row.appendChild(buttuncont)
+}
