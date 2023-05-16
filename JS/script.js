63325d3c5126e6d768fec6d3d99d908b715b0dab
AmrAbrazek <amr.3brazek@gmail.com>
2023-05-16 18:22:51 +0300

    getting the selected char

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-document.getElementsByClassName("")
+
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e => {
+        if (e.target.matches(selector)) callback (e) 
+    })
+}
+
+addGlobalEventListener('click', ".btnx", e => {
+    clicked = e.target
+    value = clicked.innerText;
+    console.log(value)
+})
